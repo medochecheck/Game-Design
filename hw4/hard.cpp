c760cec8597894382b1b11b81ee4e7b2b1b90d65
Ekaterina Chabanova <106866835+medochecheck@users.noreply.github.com>
2024-10-26 23:14:47 +0300

    Upgrade

--- hw4/hard.cpp
+++ hw4/hard.cpp
@@ -1,67 +1,122 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
+#include <string>
+#include <Windows.h>
 #include <algorithm>
-#include <cctype>
+#include <cstdlib>
+#include <locale>
+#include <codecvt>
 
+size_t length(char* str) {
+    size_t i = 0;
+    while (*(str + i) != 0) {
+        i++;
+    }
+    return i;
+}
 
-std::string trim(const std::string& str) {
+void stringcopy(char* from, char* to) {
+    size_t i = 0;
+    while (*(from + i) != 0) {
+        *(to + i) = *(from + i);
+        i++;
+    }
+    to[i] = 0;
+}
+
+int strcompare(char *from, char *to) {
+    size_t i = 0;
+    while (*(from + i) != 0 && *(to + i) != 0) {
+        if (*(from + i) != *(to + i)) {
+            return *(from + i) - *(to + i);
+        }
+        i++;
+    }
+    return *(from + i) - *(to + i);
+}
+
+std::string trim(const std::string& str) 
+{
     size_t start = str.find_first_not_of(" \t\n\r");
     size_t end = str.find_last_not_of(" \t\n\r");
     return (start == std::string::npos) ? "" : str.substr(start, end - start + 1);
-    /*
-    if (start == std::string::npos) 
-    {
-        return "";
-    } else {
-        return str.substr(start, end - start + 1);
-    }
-    */
+
 }
 
 int main() {
+    std::setlocale(LC_ALL, "Russian");
     std::ifstream file("yevgeniy_onegin.txt");
-    /*
-    if (!file.is_open()) {
-        std::cerr << "Failed to open the file.\n";
-        return 1;
-    }
-    */
-    if (!file) 
-    {
+    if (!file) {
         std::cerr << "Failed to open the file.\n";
         return 1;
     }
 
     std::vector<std::string> lines;
     std::string line;
+    char str[128];
+    size_t n = 0;
 
-
-    while (std::getline(file, line)) {
-        
+    while (std::getline(file, line)) 
+    {
         std::string trimmed_line = trim(line);
         if (!trimmed_line.empty()) {
             lines.push_back(trimmed_line);
         }
-        
-        /*
-        line = trim(line);
-        if (!line.empty()) {
-            lines.push_back(line);
-        }
-        */
     }
 
     file.close();
-    
-    
+
+    std::ifstream input("yevgeniy_onegin.txt");
+    if (!input) {
+        std::cerr << "Failed to reopen the file.\n";
+        return 1;
+    }
+
+    while (input >> str) {
+        std::cout << str << " " << length(str) << "\n";
+        n++;
+    }
+    input.clear();
+    input.seekg(0);
+
+    char** txt = (char**)calloc(n, sizeof(char*));
+    int i = 0;
+    while (input >> str) {
+        txt[i] = (char*)calloc(length(str) + 1, sizeof(char));
+        stringcopy(str, txt[i]);
+        i++;
+    }
+    input.close();
+
+    std::cout << "Words from the file:\n";
+    for (i = 0; i < n; ++i) {
+        std::cout << txt[i] << std::endl;
+    }
+
+    for (i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (strcompare(txt[i], txt[j]) > 0) {
+                char* temp = txt[i];
+                txt[i] = txt[j];
+                txt[j] = temp;
+            }
+        }
+
+    }
+
+    std::cout << "Sorted words:\n";
+    for (i = 0; i < n; ++i) {
+        std::cout << txt[i] << std::endl;
+    }
+
+    for (i = 0; i < n; ++i) {
+        free(txt[i]);
+    }
+    free(txt);
+
     std::sort(lines.begin(), lines.end());
-    /*
-    std::sort(lines.begin(), lines.end(), [](const std::string& a, const std::string& b) {
-        return a < b; // учет регистра
-    });
-    */
+
     std::cout << "Sorted lines:\n";
     for (const auto& sorted_line : lines) {
         std::cout << sorted_line << "\n";
